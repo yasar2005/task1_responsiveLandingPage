e7206095978846f91337bafd32a20035d299d826
Yasar Arafath S <180139757+yasar2005@users.noreply.github.com>
2024-10-03 21:25:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// Change background color of the navbar on scroll
+window.addEventListener('scroll', () => {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+  
